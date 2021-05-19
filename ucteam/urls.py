3d8b87824972950500d924dab8ad1bccd271fa6e
Justin Arnold <58081163+JustinTArnold@users.noreply.github.com>
2021-05-19 09:26:33 -0400

    update

--- ucteam/urls.py
+++ ucteam/urls.py
@@ -8,7 +8,7 @@ from django.views.generic.base import TemplateView
 urlpatterns = [  
     path('admin/', admin.site.urls),  
     path('hello/', views.hello),
-    path('/', views.baseredirect),
+    path('', views.baseredirect),
     path('inventory/save_table', views.save_table, name='save_table'),
     path('home/save_todo', views.save_todo, name='save_todo'), 
     path('home/', TemplateView.as_view(template_name='home.html'), name='home'),
